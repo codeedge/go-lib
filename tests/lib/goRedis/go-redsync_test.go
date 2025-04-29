425d01fca8e91493144e42db0db95fa2d14081a2
nothing <nothing>
2025-04-29 20:06:35 +0800

    看门狗

--- tests/lib/goRedis/go-redsync_test.go
+++ tests/lib/goRedis/go-redsync_test.go
@@ -1,10 +1,12 @@
 package goredis
 
 import (
+	"fmt"
 	"github.com/go-redsync/redsync/v4"
 	"github.com/go-redsync/redsync/v4/redis/goredis/v9"
 	goredislib "github.com/redis/go-redis/v9"
 	"testing"
+	"time"
 )
 
 // redis需要初始化 仅作演示用
@@ -33,6 +35,68 @@ func Test_LockRedsync(t *testing.T) {
 	}
 
 	// Do your work that requires the lock.
+
+	// Release the lock so other processes or threads can obtain a lock.
+	if ok, err := mutex.Unlock(); !ok || err != nil {
+		panic("unlock failed")
+	}
+}
+
+// redis需要初始化 仅作演示用 自动续租
+// 【Redis】【Go】分布式锁和续租：Redis分布式锁与Redsync源码解读 https://juejin.cn/post/7233284282964770871
+func Test_ExtendContext(t *testing.T) {
+	// Create a pool with go-redis (or redigo) which is the pool redisync will
+	// use while communicating with Redis. This can also be any pool that
+	// implements the `redis.Pool` interface.
+	client := goredislib.NewClient(&goredislib.Options{
+		Addr: "localhost:6379",
+	})
+	pool := goredis.NewPool(client) // or, pool := redigo.NewPool(...)
+
+	// Create an instance of redisync to be used to obtain a mutual exclusion
+	// lock.
+	rs := redsync.New(pool)
+
+	// Obtain a new mutex by using the same name for all instances wanting the
+	// same lock.
+	mutexname := "my-global-mutex"
+	mutex := rs.NewMutex(mutexname)
+
+	// Obtain a lock for our given mutex. After this is successful, no one else
+	// can obtain the same lock (the same mutex name) until we unlock it.
+	if err := mutex.Lock(); err != nil {
+		panic(err)
+	}
+
+	// Do your work that requires the lock.
+
+	//创建一个channel，用来通知续租goroutine任务已经完成
+	done := make(chan bool)
+
+	// 开启一个goroutine，周期性地续租锁
+	go func() {
+		ticker := time.NewTicker(5 * time.Second) // 按照需求调整
+		defer ticker.Stop()
+
+		for {
+			select {
+			case <-ticker.C:
+				ok, err := mutex.Extend()
+				if err != nil {
+					fmt.Println("Failed to extend lock:", err)
+				} else if !ok {
+					fmt.Println("Failed to extend lock: not successes")
+				}
+			case <-done:
+				return
+			}
+		}
+	}()
+
+	// 执行需要锁的工作
+	time.Sleep(30 * time.Second)
+	//通知goRoutine停止续租
+	close(done)
 
 	// Release the lock so other processes or threads can obtain a lock.
 	if ok, err := mutex.Unlock(); !ok || err != nil {
