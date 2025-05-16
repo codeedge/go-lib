807f8c1f307cf4a5fbf111e676715c4dfd73cb96
nothing <nothing>
2025-05-16 22:23:43 +0800

    iter

---
+++ tests/lib/internal/iter_test.go
@@ -0,0 +1,64 @@
+package copier
+
+import (
+	"fmt"
+	"iter"
+	"maps"
+	"slices"
+	"testing"
+)
+
+// GO 编程项目如何自定义迭代器？ https://www.zhihu.com/question/589450046/answer/3536826730
+
+func Test_Iter(t *testing.T) {
+	m := map[string]string{"1": "a", "2": "b"}
+	keys := slices.Collect(maps.Keys(m))
+	values := slices.Collect(maps.Values(m))
+	fmt.Println(keys, values)
+}
+
+func Test_MyRange(t *testing.T) {
+	for value := range MyRange(1, 3) {
+		fmt.Println(value) // 1 2 3
+	}
+	fmt.Println(slices.Collect(MyRange(1, 3))) // [1 2 3]
+}
+
+func MyRange(start, end int) iter.Seq[int] {
+	return func(yield func(int) bool) {
+		for current := start; current <= end; current++ {
+			if !yield(current) {
+				return
+			}
+		}
+	}
+}
+
+func IntSeq(yield func(int) bool) {
+	for i := 0; i < 10; i++ {
+		if !yield(i) {
+			break
+		}
+	}
+}
+
+var s iter.Seq[int] = IntSeq
+
+// 其实上面的s就是下面的这种的泛型
+type IntSeq2 func(yield func(int) bool)
+
+var s2 IntSeq2 = IntSeq
+
+func Test_IntSeq(t *testing.T) {
+	s(func(v int) bool {
+		fmt.Println(v)
+		return true
+	})
+
+	// for range迭代器其实就是上面s调用方法的语法糖
+	for v := range IntSeq {
+		fmt.Println(v) // 0 1 2 3 4 5 6 7 8 9
+	}
+	// 如果要 break，脱糖后就得到了 return false。
+	// 这个设计，可谓是为了不增加一个 yield 关键字而煞费苦心。
+}
