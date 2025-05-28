76b69fe2ef6019d803ec07dccae8557d4dbf9535
nothing <nothing>
2025-05-28 20:01:47 +0800

    jetcache

--- tests/lib/jetcache-go/jetcache-go_test.go
+++ tests/lib/jetcache-go/jetcache-go_test.go
@@ -19,10 +19,6 @@ import (
 // https://juejin.cn/post/7403315125007368242
 // https://github.com/mgtv-tech/jetcache-go/blob/main/docs/CN/GettingStarted.md
 
-//func Test_1(t *testing.T) {
-//
-//}
-
 var errRecordNotFound = errors.New("mock gorm.errRecordNotFound")
 
 type object struct {
@@ -37,10 +33,26 @@ func Example_basicUsage() {
 		},
 		Password: "123456",
 	})
-
 	mycache := cache.New(cache.WithName("any"),
 		cache.WithRemote(remote.NewGoRedisV9Adapter(ring)),
+		// 两种本地缓存：高频小对象用TinyLFU，低频大对象用FreeCache
+
+		// 实践方案示例
+		// 场景：电商平台商品详情缓存
+		// 热销商品元数据（平均2KB）：
+		// local.NewTinyLFU(50000) // 可缓存5万商品≈100MB
+		// 商品详情HTML片段（平均50KB）：
+		// local.NewFreeCache(512*local.MB) // 支持约1万条目
+
+		// 设置本地缓存过期时间统一为1分钟
+		// FreeCache 优点：Zero GC高性能 缺点：缓存Key/Value大小限制，需要预申请虚拟内存 最多256M内存本地缓存，
+		// 启动就先分配 采用分段环形存储结构（3个segment），当总数据量超256MB时：1. 触发近似LRU淘汰，覆盖最旧数据 2. 写入速度不受影响（无GC停顿）
+		// 条目大小限制：单个Key+Value不得超过总容量的1/1024（256MB场景下单条≤256KB）
+		// 适合存储序列化后的JSON/Protobuf等大块数据（需确保单条不超限）
 		cache.WithLocal(local.NewFreeCache(256*local.MB, time.Minute)),
+		// TinyLFU 优点：缓存命中率高 缺点：如果缓存条目过多，GC负担会比较严重 最多10000条本地缓存，
+		// 当条目数超过10000时，触发W-TinyLFU分层淘汰机制 1. 使用Count-Min Sketch统计访问频率 2. 优先淘汰低频访问的旧数据（结合LRU窗口+SLRU主区）
+		// cache.WithLocal(local.NewTinyLFU(10000, time.Minute)),
 		cache.WithCodec(json2.Name),
 		cache.WithErrNotFound(errRecordNotFound))
 
@@ -69,15 +81,23 @@ func Example_advancedUsage() {
 		Password: "123456",
 	})
 
+	// 自动刷新缓存
+	// jetcache-go提供了自动刷新缓存的能力，目的是为了防止缓存失效时造成的雪崩效应打爆数据库。对一些key比较少，实时性要求不高，加载开销非常大的缓存场景，适合使用自动刷新。
+	// 下面的代码指定每分钟刷新一次，1小时如果没有访问就停止刷新。
+	// 如果缓存是redis或者多级缓存最后一级是redis，缓存加载行为是全局唯一的，也就是说不管有多少台服务器，同时只有一个服务器在刷新，目的是为了降低后端的加载负担。
+
 	mycache := cache.New(cache.WithName("any"),
 		cache.WithRemote(remote.NewGoRedisV9Adapter(ring)),
 		cache.WithLocal(local.NewFreeCache(256*local.MB, time.Minute)),
 		cache.WithErrNotFound(errRecordNotFound),
-		cache.WithRefreshDuration(time.Minute))
+		cache.WithRefreshDuration(time.Minute),          // 设置异步刷新时间间隔
+		cache.WithStopRefreshAfterLastAccess(time.Hour), // 设置缓存 key 没有访问后的刷新任务取消时间
+	)
 
 	ctx := context.TODO()
 	key := "mykey:1"
 	obj := new(object)
+	// `Once` 接口通过 `cache.Refresh(true)` 开启自动刷新
 	if err := mycache.Once(ctx, key, cache.Value(obj), cache.TTL(time.Hour), cache.Refresh(true),
 		cache.Do(func(ctx context.Context) (any, error) {
 			return mockDBGetObject(ctx, 1)
